e2a129aec1bc4a81cd5f202dce824cd4581ae226
misscidella <nyamandevr4@gmail.com>
2024-05-05 12:07:14 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const about=document.getElementById("about");
+const projects=document.getElementById("projects");
+const contact=document.getElementById("contact");
+const home=document.getElementById("home")
+const homeButton=document.getElementById("home-button")
+const scrollToSection=(sectionId)=>()=>{
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({behavior:"smooth"});
+};
+about.addEventListener("click",scrollToSection("about-section"));
+projects.addEventListener("click",scrollToSection("projects-section"));
+contact.addEventListener("click",scrollToSection("contact-section"));
+home.addEventListener("click",scrollToSection("home-section"))
+homeButton.addEventListener("click",scrollToSection("about-section"))
